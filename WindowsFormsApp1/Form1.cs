473f3aca80e334e33aff54daa6b514778355e13b
kykyryza18 <mdasha478@gmail.com>
2022-03-21 10:40:05 +0200

    first commit

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+            string str = textBox1.Text;
+            int count = Regex.Matches(str, @"[а-я]").Count;
+            textBox2.Text += count.ToString();
+
+        }
+    }
+}
